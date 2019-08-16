343b774651f7e2a7ee70f0646a954c23c14b91f5
daquan <xudaquan@ybm100.com>
2019-08-16 10:20:38 +0800

    添加资源文件

---
+++ JKDeafultView/Classes/defaultview/UIImage+JKBundle.h
@@ -0,0 +1,16 @@
+//
+//  UIImage+JKBundle.h
+//  FBSnapshotTestCase
+//
+//  Created by admin on 2019/8/16.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UIImage (JKBundle)
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ JKDeafultView/Classes/defaultview/UIImage+JKBundle.m
@@ -0,0 +1,12 @@
+//
+//  UIImage+JKBundle.m
+//  FBSnapshotTestCase
+//
+//  Created by admin on 2019/8/16.
+//
+
+#import "UIImage+JKBundle.h"
+
+@implementation UIImage (JKBundle)
+
+@end
